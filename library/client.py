84512a308d67367e9af5edca7e731775a9c14cbc
nexus4812 <powerbatteryboy@gmail.com>
2023-01-09 18:13:18 +0900

    add hash function

--- library/client.py
+++ library/client.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from typing import Optional
 
 
-@dataclass
+@dataclass(frozen=True)
 class Payload:
     tickers: str
-    period: str = 'max'
-    end:  Optional[datetime] = None
+    period: str = "max"
+    end: Optional[datetime] = None
     start: Optional[datetime] = None
 
     def get_start_as_string(self) -> None | str:
@@ -20,13 +20,25 @@ class Payload:
 
 
 class Client:
+    __cache: dict[str, pd.DataFrame] = {}
+
     @staticmethod
     def get(query: Payload) -> pd.DataFrame:
         return yf.download(
             tickers=query.tickers,
             period=query.period,
-            start=query.get_start_as_string(),
-            end=query.get_end_as_string(),
+            start=query.start,
+            end=query.end,
             interval="1d",
             group_by="ticker",
         )
+
+    def get_in_cache(self, query: Payload, show_log: bool = False) -> pd.DataFrame:
+        # キャッシュがあればそれを使う
+        query_hash = str(hash(query))
+        if query_hash in self.__cache:
+            print('Client use hash: ' + query_hash) if show_log else None
+            return self.__cache[query_hash]
+        result = self.get(query)
+        self.__cache[query_hash] = result
+        return result
